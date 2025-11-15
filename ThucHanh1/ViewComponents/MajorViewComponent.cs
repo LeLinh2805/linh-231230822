268915a021ad76b04b44f40ef47a89f8c789a6e0
LeLinh05 <linh83014@gmail.com>
2025-11-15 23:58:09 +0700

    Update code

---
+++ ThucHanh1/ViewComponents/MajorViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThucHanh1.Data;
+
+namespace ThucHanh1.ViewComponents
+{
+    public class MajorViewComponent : ViewComponent
+    {
+        SchoolContext db;
+
+        public MajorViewComponent(SchoolContext _context)
+        {
+            db = _context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var majors = db.Majors.ToList();
+            return View("RenderMajor", majors);
+        }
+    }
+    }
